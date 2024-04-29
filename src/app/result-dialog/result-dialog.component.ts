39cc3ca03245cacc53124ae850b8ed8cfd3155cc
Idan <idanco77@gmail.com>
2024-04-29 12:53:19 +0300

    fix hashtag citydleIl

--- src/app/result-dialog/result-dialog.component.ts
+++ src/app/result-dialog/result-dialog.component.ts
@@ -42,7 +42,7 @@ export class ResultDialogComponent implements OnInit{
     const totalGuesses = this.data.guesses.filter(guess => guess.name).length;
     const highest = this.getHighest();
 
-    this.results = `#Citydle-il #${daysDifference} (${day}.${month}.${year}) ${totalGuesses}/6 (${highest}%) \n${emojis}https://citydle-il.web.app/`;
+    this.results = `#CitydleIl #${daysDifference} (${day}.${month}.${year}) ${totalGuesses}/6 (${highest}%) \n${emojis}https://citydle-il.web.app/`;
   }
 
   private getHighest(): number {
