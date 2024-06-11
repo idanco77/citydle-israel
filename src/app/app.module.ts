444579a4312c8883a386556d63d7d231cdd2e180
Idan <idanco77@gmail.com>
2024-06-11 15:26:23 +0300

    adding routing and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,80 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatInputModule} from '@angular/material/input';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {GoogleMapsModule} from '@angular/google-maps';
 import {HttpClientJsonpModule, HttpClientModule} from '@angular/common/http';
-import {StatsDialogComponent} from 'src/app/stats-dialog/stats-dialog.component';
-import {MatDialogModule} from '@angular/material/dialog';
-import {IntroDialogComponent} from 'src/app/intro-dialog/intro-dialog.component';
-import { RouterOutlet} from '@angular/router';
-import {DecimalPipe, NgOptimizedImage} from '@angular/common';
-import {MatIconModule} from '@angular/material/icon';
-import {ClipboardModule} from '@angular/cdk/clipboard';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {GuessComponent} from 'src/app/shared/components/guess/guess.component';
-import {XComponent} from 'src/app/shared/components/x/x.component';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {
-  BonusLevelNumberRangesComponent
-} from 'src/app/shared/components/bonus-level-number-ranges/bonus-level-number-ranges.component';
-import {MenuComponent} from 'src/app/shared/components/menu/menu.component';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {StateService} from 'src/app/shared/services/state.service';
-import {NearestCityComponent} from 'src/app/shared/components/nearest-cities/nearest-city.component';
-import {AutocompleteCityComponent} from 'src/app/shared/components/autocomplete-city/autocomplete-city.component';
-import {TitlesComponent} from 'src/app/shared/components/titles/titles.component';
-import {GoogleMapService} from 'src/app/shared/services/google-map.service';
-import {ErrorMessageService} from 'src/app/shared/services/error-message.service';
-import {ResultsDialogComponent} from 'src/app/results-dialog/results-dialog.component';
+import {RouterModule, Routes} from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CommonModule} from '@angular/common';
+
+const routes: Routes = [
+  {path: '', loadChildren: () => import('./citydle/citydle.module').then(m => m.CitydleModule)},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    StatsDialogComponent,
-    ResultsDialogComponent,
-    IntroDialogComponent,
-    GuessComponent,
-    XComponent,
-    BonusLevelNumberRangesComponent,
-    MenuComponent,
-    NearestCityComponent,
-    AutocompleteCityComponent,
-    TitlesComponent
   ],
   imports: [
-    BrowserModule,
+    CommonModule,
     BrowserAnimationsModule,
-    MatAutocompleteModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatSnackBarModule,
-    GoogleMapsModule,
+    BrowserModule,
+    RouterModule.forRoot(routes),
     HttpClientModule,
-    HttpClientJsonpModule,
-    MatDialogModule,
-    RouterOutlet,
-    NgOptimizedImage,
-    MatIconModule,
-    ClipboardModule,
-    FontAwesomeModule,
-    MatTooltipModule,
-    MatGridListModule,
-    MatExpansionModule,
-  ],
-  providers: [
-    StateService,
-    DecimalPipe,
-    GoogleMapService,
-    ErrorMessageService
+    HttpClientJsonpModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/citydle/citydle.module.ts
@@ -0,0 +1,85 @@
+import {NgModule} from '@angular/core';
+import {CitydleComponent} from 'src/app/citydle/citydle.component';
+import {StatsDialogComponent} from 'src/app/stats-dialog/stats-dialog.component';
+import {ResultsDialogComponent} from 'src/app/results-dialog/results-dialog.component';
+import {IntroDialogComponent} from 'src/app/intro-dialog/intro-dialog.component';
+import {GuessComponent} from 'src/app/shared/components/guess/guess.component';
+import {XComponent} from 'src/app/shared/components/x/x.component';
+import {
+  BonusLevelNumberRangesComponent
+} from 'src/app/shared/components/bonus-level-number-ranges/bonus-level-number-ranges.component';
+import {MenuComponent} from 'src/app/shared/components/menu/menu.component';
+import {NearestCityComponent} from 'src/app/shared/components/nearest-cities/nearest-city.component';
+import {AutocompleteCityComponent} from 'src/app/shared/components/autocomplete-city/autocomplete-city.component';
+import {TitlesComponent} from 'src/app/shared/components/titles/titles.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {RouterModule, RouterOutlet, Routes} from '@angular/router';
+import {CommonModule, DecimalPipe, NgOptimizedImage, PercentPipe} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {StateService} from 'src/app/shared/services/state.service';
+import {GoogleMapService} from 'src/app/shared/services/google-map.service';
+import {ErrorMessageService} from 'src/app/shared/services/error-message.service';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {GoogleMapsModule} from '@angular/google-maps';
+
+const routes: Routes = [
+  {path: '', component: CitydleComponent},
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    RouterOutlet,
+    NgOptimizedImage,
+    MatIconModule,
+    ClipboardModule,
+    FontAwesomeModule,
+    MatTooltipModule,
+    MatGridListModule,
+    MatExpansionModule,
+    RouterModule.forChild(routes),
+    PercentPipe,
+    DecimalPipe,
+    GoogleMapsModule
+  ],
+  declarations: [
+    CitydleComponent,
+    StatsDialogComponent,
+    ResultsDialogComponent,
+    IntroDialogComponent,
+    GuessComponent,
+    XComponent,
+    BonusLevelNumberRangesComponent,
+    MenuComponent,
+    NearestCityComponent,
+    AutocompleteCityComponent,
+    TitlesComponent
+  ],
+  providers: [
+    StateService,
+    DecimalPipe,
+    GoogleMapService,
+    ErrorMessageService
+  ],
+  exports: [
+    CitydleComponent
+  ]
+})
+export class CitydleModule {
+
+}
